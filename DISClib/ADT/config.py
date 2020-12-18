8a5a66d465de9513b928c6a0658c86a8c4eadb87
Juan Lucas Ibarra Sierra <jl.ibarra@uniandes.edu.co>
2020-12-18 17:18:20 -0500

    Cambios realizados desde ISIS1225-Lib

--- DISClib/ADT/config.py
+++ DISClib/ADT/config.py
@@ -4,4 +4,4 @@ file_path = os.path.join(os.path.dirname(__file__), '../..')
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 sys.path.insert(0, os.path.abspath(file_path))
 
-print("Cambios realizados con Git Hooks")
+print("Cambios en Template Reto 4")
